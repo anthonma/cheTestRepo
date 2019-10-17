e8cc903d15f27c42981fb84d6c1d6ad3e986e36f
che <che@eclipse.org>
2019-10-17 12:19:26 +0000

    Added a method

--- src/mainPackage/SimpleHelloWorld.java
+++ src/mainPackage/SimpleHelloWorld.java
@@ -1,7 +1,12 @@
 class SimpleHelloWorld{
-	
-	public static void main(Stirng[] args){
-		System.out.println("Hello, World!");
-	}
-	
+
+	public static void main(String[] args){
+        System.out.println("Hello, World!");
+        new SimpleHelloWorld().doSomething();
+    }
+
+    public void doSomething(){
+        System.out.println("I've done something");
+    }
+
 }
